d6d6114d63ecf9f00d8767148911d077f3cf4428
Ashish Singh <ashishsingh4u@gmail.com>
2019-03-11 01:05:14 +0800

    feat(Modules): Library imports and exports moved (#22)
    Library imports and exports moved to separate module
    
    fixes #21

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
- /** IE9, IE10 and IE11 requires all of the following polyfills. **/
+/* IE9, IE10 and IE11 requires all of the following polyfills. */
 
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
@@ -42,7 +42,7 @@ import 'core-js/es7/array';
 /** IE10 and IE11 requires the following for the Reflect API. */
 import 'core-js/es6/reflect';
 
-/** Evergreen browsers require these. **/
+/* Evergreen browsers require these. */
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
@@ -82,7 +82,7 @@ import 'web-animations-js'; // Run `npm install --save web-animations-js`.
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 import 'hammerjs';
 
 /***************************************************************************************************
