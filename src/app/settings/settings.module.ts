60d1f54d9cb9b0d4064d6e9d542ca8ba0bcd099b
Ashish Singh <ashishsingh4u@gmail.com>
2019-03-12 02:34:30 +0800

    Settings page added (#25) (#26)
    * feat(settings): Settings page added
    
    Settings page added
    
    fixes #20
    
    * fix(routing): Link data fixed
    
    Fixed link data to point right internationalization

---
+++ src/app/settings/settings.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SettingsComponent } from './component/settings.component';
+
+@NgModule({
+  declarations: [SettingsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SettingsModule { }
